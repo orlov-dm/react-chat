2e304b6b6f3f4fd90c5d55dc7abe94df7a0b3e0a
orlov-dm <dm.ed.orlov@gmail.com>
2018-02-09 19:17:33 +0300

    feat: Basic functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
 import reducers from './reducers';
+import setupSocket from './sockets';
+import handleNewMessage from './sagas';
+import username from './utils/name';
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+const socket = setupSocket(store.dispatch, username);
+
+sagaMiddleware.run(handleNewMessage, {socket, username});
 
 ReactDOM.render(
     <Provider store={store}>
