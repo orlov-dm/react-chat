8812b3ed7af2d1833dc2a78df231b5634cd9ba7d
Dmitriy Orlov <deo@seventest.ru>
2018-02-09 18:06:26 +0300

    feat: Handle add message event

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_MESSAGE = Symbol("ADD_MESSAGE");
+export const MESSAGE_RECEIVED = Symbol("MESSAGE_RECEIVED");
+export const ADD_USER = Symbol("ADD_USER");
+export const USERS_LIST = Symbol("USERS_LIST");
